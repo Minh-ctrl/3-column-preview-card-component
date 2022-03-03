b22de591c84c4edbbf2bbb6159b47642fd136c9a
minh-ctrl <binhminh140899@gmail.com>
2022-03-03 18:56:33 +0900

    remove dist

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/3-column-preview-card-component',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
